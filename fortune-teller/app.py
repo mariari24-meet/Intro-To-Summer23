30449544738ab8759845e4894e31a62da38ef786
mariari24-meet <mariarishmawi195@gmail.com>
2024-08-22 14:32:30 +0300

    maria
    maria

--- fortune-teller/app.py
+++ fortune-teller/app.py
@@ -1,21 +1,25 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request, url_for, redirect
 import random
 app = Flask(__name__,
 template_folder='templates',
 static_folder='static')
 
-
-@app.route('/home')
+@app.route('/home' , methods=['GET', 'POST'])
 def home():
-    return render_template("home.html")
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        birth = request.form['birthmonth']
+        print(birth)
+        return redirect(url_for('fortune', b = birth))
 
-@app.route('/fortune')
-def fortune():
+@app.route('/fortune/<b>',methods=['GET', 'POST'])
+def fortune(b):
     possible_food = ["pasta", "rice", "nothing", "left overs", "pizza", "ice cream", "burgers", "eaten steak", "bad meat", "your mom's food"]
     random_item = random.choice(possible_food)
-
-    return render_template("fortune.html", item= random_item)
-
+    lenth = len(b)
+    food = possible_food[lenth]
+    return render_template("fortune.html", item= random_item, food2 = food)
 
 
 
